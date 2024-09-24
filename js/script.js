fc8d58d831ff5a553326e09719a35b72a2bdccf6
BuiDongTanDat <bdtd1ad@gmail.com>
2024-09-24 21:50:39 +0700

    upload my first blog

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const audio = document.getElementById('audio');
+const playButton = document.getElementById('play-button');
+let isAudioPlaying = false; // Biến kiểm tra trạng thái âm thanh
+
+// Handle play button click
+playButton.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.muted = false; // Unmute when playing
+        audio.play().then(() => {
+            playButton.classList.add('spin'); // Add spinning class
+        }).catch(error => {
+            console.error('Playback failed:', error);
+        });
+    } else {
+        audio.pause();
+        playButton.classList.remove('spin'); // Remove spinning class
+    }
+});
+
+// Automatically play on load (muted)
+window.onload = () => {
+    audio.play().catch(error => {
+        console.error('Autoplay failed:', error);
+    });
+};
+
+// Change background and play music
+let originalBgColor = document.body.style.backgroundColor; 
+const colorTitle = document.getElementById('color-title');
+
+document.querySelectorAll('.color-option').forEach(option => {
+    option.addEventListener('click', function() {
+        const isSelected = this.classList.contains('selected');
+        
+        document.querySelectorAll('.color-option').forEach(opt => opt.classList.remove('selected'));
+        
+        // If click to unselect
+        if (isSelected) {
+            document.body.style.backgroundColor = originalBgColor; 
+            colorTitle.style.color = ''; //set to default
+        } else {
+            this.classList.add('selected');
+            document.body.style.backgroundColor = this.getAttribute('data-color');
+            colorTitle.style.color = this.getAttribute('bgw'); 
+            
+            audio.src = this.getAttribute('data-music');
+            audio.play();
+            document.querySelector('.audio-player span').textContent = this.getAttribute('txt-music');
+            playButton.classList.add('spin');
+        }
+    });
+});
+
